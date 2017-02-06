ffd9eeb2b05c24ceb77e580abf171685551d1718
Aftab Hussain <aftab.hussain46@gmail.com>
2017-02-05 16:05:23 -0800

    formattings and comments

--- algorithms/pagerank/pagerank.hpp
+++ algorithms/pagerank/pagerank.hpp
@@ -23,133 +23,122 @@
 
 namespace algorithm {
   namespace sg_simple {
-    template <typename F>
-    class pagerank {
-    private:
+   
+   	 	template <typename F>
+    	class pagerank {
+    	private:
 
-      struct vertex {
-	vertex_t degree;
-	weight_t rank;
-	weight_t sum;
-      } __attribute__((__packed__));
+      		struct vertex {//vertex structure
+				vertex_t degree;
+				weight_t rank;
+				weight_t sum;
+        	} __attribute__((__packed__));
 
-      struct update {
-	vertex_t target;
-	weight_t rank;
-      } __attribute__((__packed__));
+      		struct update {//update structure
+				vertex_t target;
+				weight_t rank;
+      		} __attribute__((__packed__));
 
-      static const weight_t DAMPING_FACTOR = 0.85;
-      static unsigned long niters;
+      		static const weight_t DAMPING_FACTOR = 0.85;
+      		static unsigned long niters;
 
-    public:
+    	public:
     
-      static unsigned long vertex_state_bytes() {
-	return sizeof(struct vertex);
-      }
-      static unsigned long split_size_bytes() {
-	return sizeof(struct update);
-      }
+      		static unsigned long vertex_state_bytes() {
+				return sizeof(struct vertex);
+      		}
+      		static unsigned long split_size_bytes() {
+				return sizeof(struct update);
+      		}
 
-      static unsigned long split_key(unsigned char* buffer, unsigned long jump)
-      {
-	struct update* u = (struct update*)buffer;
-	vertex_t key = u->target;
-	key = key >> jump;
-	return key;
-      }
+     		static unsigned long split_key(unsigned char* buffer, unsigned long jump) {
+				struct update* u = (struct update*)buffer;
+				vertex_t key = u->target;
+				key = key >> jump;
+				return key;
+      		}
 
-      static bool init(unsigned char* vertex_state,
-		       unsigned long vertex_index,
-		       unsigned long bsp_phase,
-		       per_processor_data *cpu_state)
-      {
-	struct vertex* vertices = (struct vertex*)vertex_state;
-	vertices->sum  = 0;
-	if(bsp_phase == 0) {
-	  vertices->degree = 0;
-	  vertices->rank = 1.0;
-	}
-	else if(bsp_phase == 1) {
-	  // Properly set up source vertices (no incoming edges)
-	  vertices->rank = (1 - DAMPING_FACTOR);
-	}
-	return true;
-      }
+      static bool init(unsigned char* vertex_state, unsigned long 
+      		vertex_index, unsigned long bsp_phase, per_processor_data *cpu_state) {
+			
+			struct vertex* vertices = (struct vertex*)vertex_state;
+			vertices->sum  = 0;
+			if(bsp_phase == 0) {
+	 		 	vertices->degree = 0;
+	 		 	vertices->rank = 1.0;
+			}
+			else if(bsp_phase == 1) {
+	  			// Properly set up source vertices (no incoming edges)
+	  			vertices->rank = (1 - DAMPING_FACTOR);
+			}
+			return true;
+      	}
 
-      static bool need_init(unsigned long bsp_phase)
-      {
-	return true;
-      }
+      static bool need_init(unsigned long bsp_phase) {
+			return true;
+      	}
 
-      static void preprocessing()
-      {
-	// The extra +1 is for the iteration 0 which is used just to compute the vertex degrees
-	niters = 1 + vm["pagerank::niters"].as<unsigned long>();
-      }
+      static void preprocessing() {
+			// The extra +1 is for the iteration 0 which is used just to compute the vertex degrees
+			niters = 1 + vm["pagerank::niters"].as<unsigned long>();
+      	}
 
-      static bool apply_one_update(unsigned char* vertex_state,
-				   unsigned char* update_stream,
-				   per_processor_data* per_cpu_data,
-				   unsigned long bsp_phase)
-      {
-	struct update* u = (struct update*)update_stream;
-	struct vertex* vertices = (struct vertex*)vertex_state;
-	struct vertex* v = &vertices[x_lib::configuration::map_offset(u->target)];
+      static bool apply_one_update(unsigned char* vertex_state,unsigned char* 
+      			update_stream, per_processor_data* per_cpu_data, unsigned long bsp_phase) {
+			struct update* u = (struct update*)update_stream;
+			struct vertex* vertices = (struct vertex*)vertex_state;
+			struct vertex* v = &vertices[x_lib::configuration::map_offset(u->target)];
 
-	v->sum += u->rank;
-	v->rank = 1 - DAMPING_FACTOR + DAMPING_FACTOR * v->sum;
+			v->sum += u->rank;
+			v->rank = 1 - DAMPING_FACTOR + DAMPING_FACTOR * v->sum;
 
-	if (bsp_phase == niters)
-	  return false;
-	else
-	  return true;
-      }
+			if (bsp_phase == niters)// stop returning updates in the final iteration
+	  			return false;
+			else
+	  		return true;
+      	}
 
       static bool generate_update(unsigned char* vertex_state,
 				  unsigned char* edge_format,
 				  unsigned char* update_stream,
 				  per_processor_data* per_cpu_data,
-				  unsigned long bsp_phase)
-      {
-	vertex_t src, dst;
-	F::read_edge(edge_format, src, dst);
+				  unsigned long bsp_phase){
+		vertex_t src, dst;
+		F::read_edge(edge_format, src, dst);
 
-	struct vertex* vertices = (struct vertex*)vertex_state;
-	struct vertex* v = &vertices[x_lib::configuration::map_offset(src)];
+		struct vertex* vertices = (struct vertex*)vertex_state;
+		struct vertex* v = &vertices[x_lib::configuration::map_offset(src)];
 
-	if (bsp_phase == 0) {
-	  v->degree++;
-	  return false;
-	}
-	else {
-	  struct update* u = (struct update*)update_stream;
-	  u->target = dst;
-	  if(bsp_phase == 1) {
-	    u->rank = 1.0/v->degree;
-	  }
-	  else {
-	    u->rank = v->rank / v->degree;
-	  }
-	  return true;
-	}
+		if (bsp_phase == 0) {//increment vertex degrees in the first iteration
+		  v->degree++;
+		  return false;
+		}
+		else {// if not the 0th phase
+	 		 struct update* u = (struct update*)update_stream;
+	 		 u->target = dst;
+	  			if(bsp_phase == 1) {// in the first iteration.
+	   			 u->rank = 1.0/v->degree;
+	  			}
+	  			else {//in other iterations
+	    			u->rank = v->rank / v->degree;
+	  			}
+	  		return true;
+		}
       }
 
-      static per_processor_data* create_per_processor_data(unsigned long processor_id)
-      {
-	return NULL;
-      }
+      static per_processor_data* create_per_processor_data(unsigned long processor_id){
+			return NULL;
+      	}
 
-      static unsigned long min_super_phases()
-      {
-	return 2;
-      }
+      static unsigned long min_super_phases(){
+			return 2;
+      	}
     
       static void postprocessing() {}
-
     };
 
     template<typename F>
-    unsigned long pagerank<F>::niters;
+    unsigned long pagerank<F>::niters;//number of iterations is pre specified.
   }
 }
 #endif
